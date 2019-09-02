06179ed7c572419b32f5c4fad7960e8884433b62
Igor Cherevko <ich.hideez@gmail.com>
2019-09-02 16:53:35 +0300

    Task extensions

---
+++ DotNet.Utility/TaskExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNet.Utility
+{
+    public static class TaskExtension
+    {
+        public static void ForgetSafely(this Task task, Action<Exception> handleException)
+        {
+            task.ContinueWith(t =>
+            {
+                if (task.IsFaulted)
+                {
+                    handleException.Invoke(task.Exception);
+                }
+            });
+        }
+
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+
+            using (var timeoutCancellationTokenSource = new CancellationTokenSource())
+            {
+
+                var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+                if (completedTask == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    return await task;
+                }
+                else
+                {
+                    throw new TimeoutException("The operation has timed out.");
+                }
+            }
+        }
+    }
+}
